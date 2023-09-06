63b4d5c4837d13782850e5eec9d21e67e0004126
rouslopp <romina.lopez@estudiantes.utec.edu.uy>
2023-09-06 16:19:31 -0300

    practico de clase

---
+++ Practico2/DataAccessLayer/DALs/DAL_Personas_ADONet.cs
@@ -0,0 +1,121 @@
+﻿using DataAccessLayer.IDALs;
+using Microsoft.Data.SqlClient;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.DALs
+{
+    public class DAL_Personas_ADONet : IDAL_Personas
+    {
+        private string _connectionString = "Server=localhost,1433;Database=practico2;User Id=sa;Password=Abc*123!;Encrypt=False;";
+        public void Delete(string documento)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                Persona persona = new Persona();
+
+                using (SqlCommand cmd = new SqlCommand("DELETE * FROM personas WHERE Documento = @documento", connection))
+                {
+                    cmd.Parameters.AddWithValue("@documento", documento);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public List<Persona> Get()
+        {
+            //Instancio la lista a devolver
+            List<Persona> res = new List<Persona>();
+
+            //using -> para definir un contexto relacionado a un recurso y asegurarnos de que se cierre luego de usarse
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                
+                using(SqlCommand cmd = new SqlCommand("SELECT * FROM personas", connection))
+                {
+                    using(SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while(reader.Read())
+                        {
+                            Persona persona = new Persona();
+
+                            persona.Documento = reader["Documento"].ToString();
+                            persona.Nombre = reader["Nombre"].ToString();
+
+                            res.Add(persona);
+                        }
+                    }
+
+                }
+            }
+
+
+            return res;
+
+        }
+
+        public Persona Get(string documento)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                Persona persona = new Persona();
+
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM personas WHERE Documento = @documento", connection))
+                {
+                    cmd.Parameters.AddWithValue("@documento", documento);
+
+                    using( SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        persona.Documento = reader["Documento"].ToString();
+                        persona.Nombre = reader["Nombre"].ToString();
+
+                        return persona;
+                    }
+                }
+            }
+        }
+
+        public void Insert(Persona persona)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO personas (Documento, Nombre) VALUES (@documento, @nombre)", connection))
+                {
+                    cmd.Parameters.AddWithValue("@documento", persona.Documento);
+                    cmd.Parameters.AddWithValue("@nombre", persona.Nombre);
+
+                    cmd.ExecuteNonQuery();
+                }
+                
+            }
+        }
+
+        public void Update(Persona persona)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand cmd = new SqlCommand("UPDATE personas SET Nombre = @nombre WHERE Documento = @documento", connection))
+                {
+                    cmd.Parameters.AddWithValue("@documento", persona.Documento);
+                    cmd.Parameters.AddWithValue("@nombre", persona.Nombre);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ Practico2/PracticoClase1/Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Shared;
+using DataAccessLayer.DALs;
+using DataAccessLayer.IDALs;
+using BusinessLayer.IBLs;
+using BusinessLayer.BLs;
+using PracticoClase1;
+
+Console.WriteLine("Primera Aplicación con .NET");
+
+IDAL_Personas _personas = new DAL_Personas_ADONet();
+IBL_Personas _personasBL = new BL_Personas(_personas);
+Commands commands = new Commands(_personasBL);
+
+Console.WriteLine("Comandos Posibles:");
+Console.WriteLine("1 - Agregar Persona");
+Console.WriteLine("2 - Listar Personas");
+Console.WriteLine("3 - Eliminar Persona");
+Console.WriteLine("4 - Mostrar Persona");
+Console.WriteLine("5 - Actualizar Persona");
+Console.WriteLine("6 - Salir");
+
+Console.Write("Ingrese Comando> ");
+string command = Console.ReadLine();
+
+while(command != "6")
+{
+    try
+    {
+        switch (command)
+        {
+            case "1":
+                commands.AddPersona();
+                break;
+            case "2":
+                commands.ListPersonas();
+                break;
+            case "3":
+                commands.RemovePersona();
+                break;
+            case "4":
+                commands.MostrarPersona();
+                break;
+            case "5":
+                commands.UpdatePersona();
+                break;
+            default:
+                Console.WriteLine("Comando no reconocido");
+                break;
+        }
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine("ERROR --> " + ex.Message);
+    }
+    finally
+    {
+        Console.Write("Ingrese Comando > ");
+        command = Console.ReadLine();
+    }
+}
+
+Console.WriteLine("Gracias por usar la aplicación");
