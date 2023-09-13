cd4941b29ccba682cfe7e709ff7cb8311ce35032
rouslopp <romina.lopez@estudiantes.utec.edu.uy>
2023-09-13 17:50:26 -0300

    crud personas con insert d evehiculos, practico 3

---
+++ Practico3/DataAccessLayer/Migrations/20230906224511_AgregandoDireccionYFechaNacimiento.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregandoDireccionYFechaNacimiento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Direccion",
+                table: "Personas",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaNacimiento",
+                table: "Personas",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Direccion",
+                table: "Personas");
+
+            migrationBuilder.DropColumn(
+                name: "FechaNacimiento",
+                table: "Personas");
+        }
+    }
+}
